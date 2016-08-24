009623f8859bb747dc452a82ebec90b099a8c264
Blake Steel <BlakeMSteel@gmail.com>
2016-08-24 11:01:30 -0400

    port methods from heroku tutorial 7

--- index.js
+++ index.js
@@ -21,3 +21,13 @@ io.on('connection', function(socket){
 		io.emit('chat message', msg);
 	});
 });
+
+		var socket = io();
+		$('form').submit(function(){
+			socket.emit('chat message', $('#m').val());
+			$('#m').val('');
+			return false;
+		});
+		socket.on('chat message', function(msg){
+			$('#messages').append($('<li>').text(msg));
+		});
